ff94058f4f861a43a4af6d55b1f97079325b7d64
christian-fortin <christian.ep.410@gmail.com>
2022-02-09 16:53:42 -0500

    Took away blocking boostrap. placed logo, username, today's date, search bar. WIll work on finishing topbar, and then nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Feed from './components/Feed';
 import Sidebar from './components/Sidebar';
@@ -8,7 +8,7 @@ import TopBar from './components/TopBar';
 
 function App() {
   return (
-    <div className='bg-dark container-flex'>
+    <div className='bg-dark container-flex' id="backgroundColor">
       <TopBar />
       <div className='row'>
         <div className='col-2'>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,9 +4,6 @@ const Sidebar = () => {
   return (
     <>
       <h4 className='text-muted'>Fun Times</h4>
-      <form action=''>
-        <input type='text' placeholder='Search LoFive' />
-      </form>
     </>
   );
 };
