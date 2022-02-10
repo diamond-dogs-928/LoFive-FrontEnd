6436358104dbfed02dfa257166a29ff5e6b3a581
Justin Herpin <justindherpin@yahoo.com>
2022-02-10 14:07:57 -0800

    still working on CreatePost.  committing before taking a break.

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -2,23 +2,40 @@ import { useState } from 'react'
 
 const CreatePost = () => {
 
-  const NewForm = () => {
     const [name, setName] = useState('')
+    const [note, setNote] = useState('')
 
-  }
+    let handleSubmit = (e) => {
+
+      e.preventDefault()
+
+      fetch('http://localhost:4000/notes')
+
+    }
+
+    let handleChange = (e) => {
+      setName(e.target.value)
+    }
 
   return (
     <>
       <form action='' id='createPost'>
 
         <label htmlFor='name'>Name</label>
-        <input type='text' id="name" name="name" placeholder='Name' />
+        <input 
+          type='text' 
+          id="name" 
+          name="name" 
+          value={name} 
+          placeholder='Name' 
+          onChange={ handleChange }
+        />
 
         <label htmlFor='note'>Post</label>
         <input type='text' id="note" name="note" placeholder='say something' />
 
         <input type="submit" value="Post" />
-        
+
       </form>
     </div>
   );
