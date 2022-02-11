bab5d2f1afe6c13ede50560ac23e7355df4c0302
garrigoose <gkats11@gmail.com>
2022-02-11 11:04:57 -0500

    latest feature commit

--- src/components/Post.js
+++ src/components/Post.js
@@ -25,8 +25,24 @@ const Post = () => {
         harum?
       </p>
       <p className='text-muted'>
-        <a href=''>tag</a> <a href=''>tag</a> <a href=''>tag</a>{' '}
-        <a href=''>tag</a> <a href=''>tag</a>
+        <button>
+          <a href=''>tag</a>
+        </button>{' '}
+        <button>
+          <a href=''>tag</a>
+        </button>{' '}
+        <button>
+          <a href=''>tag</a>
+        </button>{' '}
+        <button>
+          <a href=''>tag</a>
+        </button>{' '}
+        <button>
+          <a href=''>tag</a>
+        </button>{' '}
+        <button>
+          <a href=''>tag</a>
+        </button>{' '}
       </p>
       <h5 className='d-flex justify-content-between text-muted'>
         Date &amp; Time <span>⬆️ Likes</span>
