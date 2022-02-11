cd0ea97130a651549be86ae995db6d4ee08d2a58
garrigoose <gkats11@gmail.com>
2022-02-11 11:36:42 -0500

    removed one stupid line

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 const CreatePost = () => {
   return (
-    // <div className='create-post'>
-    //   <form action=''>
-    //     <input type='text' placeholder='say something' />
-    //   </form>
     <div>
       <form className='CreatePost' action=''>
         <input id='SearchBox' type='text' placeholder='Search LoFive' />
