8af39c8043ffbabe11ae0c1be8708d85488cad3d
Justin Herpin <justindherpin@yahoo.com>
2022-02-11 22:02:16 -0800

    Fetch to call Notes from backend pulling data and logging to console correctly

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,8 +2,25 @@ import React from 'react';
 import Post from './Post';
 import CreatePost from './CreatePost';
 import '../CSS/feed.css';
+import { useState, useEffect } from 'react';
 
 const Feed = () => {
+
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+
+    fetch('http://localhost:4000/notes')  
+
+    .then(data => data.json())
+
+    .then((parsedData) => {
+      console.log(parsedData);
+      setNotes(parsedData)
+    })
+
+  }, [])
+
   return (
     <div>
       <div>
