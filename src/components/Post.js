7447bd3a2df899d6af0802101a310ec15ca0287a
christian-fortin <christian.ep.410@gmail.com>
2022-02-15 11:59:56 -0500

    Middle of working on feed. Styled post textarea's. Pushing for co-workers to work on now

--- src/components/Post.js
+++ src/components/Post.js
@@ -9,17 +9,80 @@ const Post = ({ note, name, likes, tags, addLike, date }) => {
   // console.log(postDate);
 
   return (
-    <div className='card bg-dark textmuted post'>
-      <h4 className='text-muted'>Author: {name}</h4>
-      <p className='text-muted'>{note}</p>
+    <div className='post'>
+    
+          <div className="card">
+                  <div className="card-header">
+                      <img src="icon2Solo.png" alt="###"/>
+                      <div>
+                          <p>Author: {name} {/* (username)*/}</p> 
+                          <p>{note} {/*(Name? Name of song?)*/}</p>
+                      </div>
+                  </div>
+                  <div className="card-body">
+                    {/* Song Cover? */}
+                      <img src="/../images/icon2Solo.png" alt=""/>
+                  </div>
+                  <div className="card-footer">
+                      <i className="fas fa-heart" >{/* LIKES*/}</i>
+                      <i className="fas fa-comment"> {/* Comments*/}</i>
+                      <i className="fas fa-bookmark"> {/* SHARE*/}</i>
 
-      {tags.map((value, index) => {
-        return (
-          <button key={index}>
-            <a href='#'>{value}</a>
-          </button>
-        );
-      })}
+                  </div>
+              </div>
+
+
+  </div>
+
+
+
+
+
+
+
+  );
+};
+
+export default Post;
+
+
+
+
+// <h4 className='text-muted'>Author: {name}</h4>
+// <p className='text-muted'>{note}</p>
+
+// {tags.map((value, index) => {
+//   return (
+//     <button key={index}>
+//       <a href='#'>{value}</a>
+//     </button>
+//   );
+// })}
+
+// <div className='postBottom'>
+//   {date}
+//   <span className='likeBlock'>
+//     <p className='likeButton' onClick={() => addLike(note)}>
+//     <span>Likes: {likes}</span>
+//     </p>{' '}
+//   </span>
+//   {/* Date &amp; Time <span><button onClick={ incrementLikes }>⬆️</button> Likes: {likes}</span> */}
+// </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
       {/* <p className='text-muted'>
         <button>
@@ -41,17 +104,3 @@ const Post = ({ note, name, likes, tags, addLike, date }) => {
           <a href=''>tag</a>
         </button>{' '}
       </p> */}
-      <div className='postBottom'>
-        {date}
-        <span className='likeBlock'>
-          <p className='likeButton' onClick={() => addLike(note)}>
-            ⬆️ <span>Likes: {likes}</span>
-          </p>{' '}
-        </span>
-        {/* Date &amp; Time <span><button onClick={ incrementLikes }>⬆️</button> Likes: {likes}</span> */}
-      </div>
-    </div>
-  );
-};
-
-export default Post;
