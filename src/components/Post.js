d07dec5c83e153182a9e00f508ad3245af000d00
christian-fortin <christian.ep.410@gmail.com>
2022-02-15 20:42:40 -0500

    more work on the feed cards, and some small edits to other elements.

--- src/components/Post.js
+++ src/components/Post.js
@@ -10,18 +10,9 @@ const Post = ({ post, note, name, likes, tags, addLike, date, deleteNote }) => {
 
   return (
     <div>
-      <a href="#" class="data-card">
+      <a href='' class='data-card'>
+      {/* Make the href the users profile --- Show route goes here*/}
         <div className="postBottom">
-          {date}
-          <span className="likeBlock">
-            {/* <p className="likeButton" onClick={() => addLike(note)}> */}
-            <p className="likeButton" onClick={() => addLike(post)}>
-              ⬆️ <span>Likes: {likes}</span>
-            </p>{' '}
-            {/* <p className="likeButton" onClick={() => addLike(note)}>
-              <button onClick={() => addLike(note) }>⬆️</button><span>Likes: {likes}</span>
-            </p>{' '} */}
-          </span>
           {/* Date &amp; Time <span><button onClick={ incrementLikes }>⬆️</button> Likes: {likes}</span> */}
         </div>
         <h4>{note}</h4>
@@ -34,9 +25,18 @@ const Post = ({ post, note, name, likes, tags, addLike, date, deleteNote }) => {
               </button>
             );
           })}
+          <span className="likeBlock">
+            {/* <p className="likeButton" onClick={() => addLike(note)}> */}
+            <p className="likeButton" onClick={() => addLike(post)}>
+              ⬆️ <span>Likes: {likes}</span>
+            </p>{' '}
+            {/* <p className="likeButton" onClick={() => addLike(note)}>
+              <button onClick={() => addLike(note) }>⬆️</button><span>Likes: {likes}</span>
+            </p>{' '} */}
+          </span>
         </p>
         <span class="link-text">
-          edit
+          Edit
           <svg
             width="25"
             height="16"
@@ -48,12 +48,12 @@ const Post = ({ post, note, name, likes, tags, addLike, date, deleteNote }) => {
               fill-rule="evenodd"
               clip-rule="evenodd"
               d="M17.8631 0.929124L24.2271 7.29308C24.6176 7.68361 24.6176 8.31677 24.2271 8.7073L17.8631 15.0713C17.4726 15.4618 16.8394 15.4618 16.4489 15.0713C16.0584 14.6807 16.0584 14.0476 16.4489 13.657L21.1058 9.00019H0.47998V7.00019H21.1058L16.4489 2.34334C16.0584 1.95281 16.0584 1.31965 16.4489 0.929124C16.8394 0.538599 17.4726 0.538599 17.8631 0.929124Z"
-              fill="#753BBD"
+              fill="#701555"
             />
           </svg>
         </span>
-        <span onClick={() => deleteNote(post) } class="link-text">
-          delete
+        <span onClick={() => deleteNote(post)} class="link-text">
+          Delete
           <svg
             width="25"
             height="16"
@@ -65,10 +65,11 @@ const Post = ({ post, note, name, likes, tags, addLike, date, deleteNote }) => {
               fill-rule="evenodd"
               clip-rule="evenodd"
               d="M17.8631 0.929124L24.2271 7.29308C24.6176 7.68361 24.6176 8.31677 24.2271 8.7073L17.8631 15.0713C17.4726 15.4618 16.8394 15.4618 16.4489 15.0713C16.0584 14.6807 16.0584 14.0476 16.4489 13.657L21.1058 9.00019H0.47998V7.00019H21.1058L16.4489 2.34334C16.0584 1.95281 16.0584 1.31965 16.4489 0.929124C16.8394 0.538599 17.4726 0.538599 17.8631 0.929124Z"
-              fill="#753BBD"
+              fill="#701555"
             />
           </svg>
         </span>
+        <div className="cardDateDiv">{date}</div>
       </a>
     </div>
   );
