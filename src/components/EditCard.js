113f931605bffb81eb739a1e6a5d5e3d228560ef
Justin Herpin <justindherpin@yahoo.com>
2022-02-17 20:18:46 -0800

    working on EditCard.  going to sleep.

--- src/components/EditCard.js
+++ src/components/EditCard.js
@@ -1,52 +1,93 @@
-import React from 'react'
+import { useParams } from 'react-router-dom';
+import {useEffect, useState} from 'react'
+
+
 
 const EditCard = () => {
-  return (
-    <>
-    {/* <form onSubmit={handleSubmit} id='createPost'>
-      <label htmlFor='name'></label>
-      <textarea
-        type='text'
-        id='name'
-        name='name'
-        value={formState.name}
-        placeholder='Name'
-        onChange={handleChange}
-        className='createPostNameInput'
-      />
 
-      <label htmlFor='note'></label>
-      <textarea
-        rows='5'
-        type='text'
-        id='note'
-        name='note'
-        value={formState.note}
-        placeholder='Say something'
-        onChange={handleChange}
-        className='createPostInput'
-      />
+  const initialState = { name: '', note: '', tags: [] };
+  const [formState, setFormState] = useState(initialState);
+  const [doc, setdoc] = useState()
+  const { id } = useParams()
 
-      <label htmlFor='tags'></label>
-      <textarea
-        rows='5'
-        type='text'
-        id='tags'
-        name='tags'
-        value={formState.tags}
-        placeholder='Tag IT!'
-        onChange={handleChange}
-        className='createPostInput'
-      />
+  let getNote = async (id) => {
+    let data = await fetch('http://localhost:4000/notes/' + id);
+    let json = await data.json();
+    if (json) {
+      setdoc(json);
+    }
+  };
+
+  useEffect(() => {
+    getNote();
+  }, []);
+
+  const options = {
+    method: 'PUT',
+    body: JSON.stringify({ owner: doc.name, post: doc.note, tags: doc.tags }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+  
+  let editCard = async (note) => {
+    let data = await fetch('http://localhost:4000/notes/' + id, options) 
+    let json = await data.json()
+    if (json) {
+      //addNote(resJson);
+      //console.log(resJson)
+      //setFormState(initialState);
+    }
+
+      //     .catch((error) => console.error({ Error: error }));
+      // };
+
+  // return (
+  //   <>
+  //   <form onSubmit={handleSubmit} id='createPost'>
+  //     <label htmlFor='name'></label>
+  //     <textarea
+  //       type='text'
+  //       id='name'
+  //       name='name'
+  //       value={formState.name}
+  //       placeholder='Name'
+  //       onChange={handleChange}
+  //       className='createPostNameInput'
+  //     />
+
+  //     <label htmlFor='note'></label>
+  //     <textarea
+  //       rows='5'
+  //       type='text'
+  //       id='note'
+  //       name='note'
+  //       value={formState.note}
+  //       placeholder='Say something'
+  //       onChange={handleChange}
+  //       className='createPostInput'
+  //     />
+
+  //     <label htmlFor='tags'></label>
+  //     <textarea
+  //       rows='5'
+  //       type='text'
+  //       id='tags'
+  //       name='tags'
+  //       value={formState.tags}
+  //       placeholder='Tag IT!'
+  //       onChange={handleChange}
+  //       className='createPostInput'
+  //     />
 
 
 
-      <button type='submit' value='Post' class='button-56'>
-        <strong>Post</strong>
-      </button>
-    </form> */}
-  </>
-  )
-}
+  //     <button type='submit' value='Post' class='button-56'>
+  //       <strong>Post</strong>
+  //     </button>
+  //   </form>
+  // </>
+  // )
+  }}
 
-export default EditCard
+export default EditCard;
